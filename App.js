d6f9840a660289c996707b939699129d8c69efa8
Akash Deep Mishra <akashdeepmishra264.etw@gmail.com>
2023-01-07 00:36:26 +0530

    changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const drag = (event) => {
   setTimeout(() => {
     event.target.className = " hide";
   }, 0);
-}; 
+};
 const dragEnd = (event) => {
   event.target.className = " taskpara";
 };
@@ -20,6 +20,9 @@ let taskb = document.querySelector(".btn1");
 taskb.addEventListener("click", (e) => {
   let copytask = document.getElementById("copyText").value;
   let para = document.createElement("li");
+  let para2=document.createElement("textarea");
+  para2.setAttribute("type","text");
+  para2.className="taskpara";
   para.textContent = copytask;
   para.className = "taskpara";
   para.setAttribute("draggable", "true");
@@ -30,6 +33,7 @@ taskb.addEventListener("click", (e) => {
   para.addEventListener("dragend", dragEnd);
   let addpara = document.getElementById("listpara");
   addpara.appendChild(para);
+  para.appendChild(para2);
 });
 
 //drop the task
